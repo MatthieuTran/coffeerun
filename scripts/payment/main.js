910d533437c819183ec05f4f281e1f2e8531c0e9
Matthieu Tran <matthieuktran@gmail.com>
2020-09-25 19:09:55 -0700

    Add files

---
+++ scripts/payment/main.js
@@ -0,0 +1,20 @@
+(function (window) {
+  "use strict";
+  var FORM_SELECTOR = '[data-payment-order="form"]';
+
+  var App = window.App;
+  var FormHandler = App.FormHandler;
+
+  var formHandler = new FormHandler(FORM_SELECTOR);
+  var modalSelector = $("#ex1");
+
+  formHandler.addSubmitHandler(function (data) {
+      console.log(data)
+
+      modalSelector.text('Thank you for your payment, ' + data.contactTitle + ' ' + data.username)
+      modalSelector.appendTo("body").modal();
+  });
+
+  console.log(formHandler);
+  console.log(modalSelector)
+})(window);
